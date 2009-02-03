1517874032eadfa21c758d868956ef42dfe51f44
aldaris <aldaris@81257025-f8dc-45f4-8694-d92bdbe95a3d>
2009-02-03 17:56:08 +0000

    #102 improved display
    git-svn-id: https://maszat.sch.bme.hu/svn/vir-dev/trunk/sch-kp@464 81257025-f8dc-45f4-8694-d92bdbe95a3d

--- sch-kp-web/src/main/java/hu/sch/kp/web/pages/group/GroupHistory.java
+++ sch-kp-web/src/main/java/hu/sch/kp/web/pages/group/GroupHistory.java
@@ -4,15 +4,13 @@
  */
 package hu.sch.kp.web.pages.group;
 
-import hu.sch.domain.BelepoIgeny;
 import hu.sch.domain.Csoport;
 import hu.sch.domain.Ertekeles;
-import hu.sch.domain.PontIgeny;
 import hu.sch.domain.Szemeszter;
 import hu.sch.kp.services.ErtekelesManagerLocal;
 import hu.sch.kp.services.UserManagerLocal;
+import hu.sch.kp.web.pages.ertekeles.ErtekelesDetailPanel;
 import hu.sch.kp.web.pages.index.Index;
-import hu.sch.kp.web.pages.user.ShowUser;
 import hu.sch.kp.web.templates.SecuredPageTemplate;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -20,16 +18,10 @@ import java.util.Iterator;
 import java.util.List;
 import javax.ejb.EJB;
 import org.apache.wicket.PageParameters;
-import org.apache.wicket.markup.html.WebMarkupContainer;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
-import org.apache.wicket.markup.html.link.Link;
-import org.apache.wicket.markup.html.list.ListItem;
-import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.CompoundPropertyModel;
-import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.PropertyModel;
 
 /**
@@ -43,13 +35,12 @@ public class GroupHistory extends SecuredPageTemplate {
     @EJB(name = "UserManagerBean")
     UserManagerLocal userManager;
     private List<Ertekeles> ertekelesList = new ArrayList<Ertekeles>();
-    private Ertekeles selectedErtekeles = null;
     private Long id;
     private Csoport csoport;
     private Szemeszter szemeszter = null;
     private String selected = "";
-    private List<PontIgeny> pontIgenyek = new ArrayList<PontIgeny>();
-    private List<BelepoIgeny> belepoIgenyek = new ArrayList<BelepoIgeny>();
+    private Ertekeles selectedErtekeles = null;
+    private ErtekelesDetailPanel ertekelesPanel;
 
     public GroupHistory(PageParameters parameters) {
         Object p = parameters.get("id");
@@ -72,7 +63,6 @@ public class GroupHistory extends SecuredPageTemplate {
             szemeszterek.add(((Ertekeles) iterator.next()).getSzemeszter().toString());
         }
         Collections.sort(szemeszterek);
-        updatePage();
         Form idoszakForm = new Form("idoszakForm") {
 
             @Override
@@ -82,7 +72,9 @@ public class GroupHistory extends SecuredPageTemplate {
                     selectedErtekeles = (Ertekeles) iterator.next();
                     szemeszter = selectedErtekeles.getSzemeszter();
                     if (szemeszter.toString().equals(selected)) {
-                        updatePage();
+                        setHeaderLabelText(csoport.getNev() + " részletes pontozásai");
+                        ertekelesPanel.updateDatas(selectedErtekeles);
+                        ertekelesPanel.setVisible(true);
                         break;
                     }
                 }
@@ -94,70 +86,11 @@ public class GroupHistory extends SecuredPageTemplate {
 
         idoszakForm.add(ddc);
         add(idoszakForm);
-        generatePointTable();
-        generateEntrantTable();
-    }
+        setModel(new CompoundPropertyModel(selectedErtekeles));
 
-    public void generatePointTable() {
-        WebMarkupContainer pontTable = new WebMarkupContainer("pontTabla");
-        ListView pontListView = new ListView("pontok", pontIgenyek) {
-
-            @Override
-            protected void populateItem(ListItem item) {
-                final PontIgeny p = (PontIgeny) item.getModelObject();
-                IModel model = new CompoundPropertyModel(p);
-                item.setModel(model);
-                Link felhasznaloLink = new Link("felhLink") {
-
-                    @Override
-                    public void onClick() {
-                        setResponsePage(ShowUser.class,
-                                new PageParameters("id=" + p.getFelhasznalo().getId().toString()));
-                    }
-                };
-                felhasznaloLink.add(new Label("felhNev", new PropertyModel(p, "felhasznalo.nev")));
-                item.add(felhasznaloLink);
-                item.add(new Label("pontszam", new PropertyModel(p, "pont")));
-            }
-        };
-        pontTable.add(pontListView);
-        add(pontTable);
-    }
-
-    public void generateEntrantTable() {
-        WebMarkupContainer belepoTable = new WebMarkupContainer("belepoTabla");
-        ListView belepoListView = new ListView("belepok", belepoIgenyek) {
-
-            @Override
-            protected void populateItem(ListItem item) {
-                final BelepoIgeny b = (BelepoIgeny) item.getModelObject();
-                IModel model = new CompoundPropertyModel(b);
-                item.setModel(model);
-                Link felhasznaloLink = new Link("felhLink") {
-
-                    @Override
-                    public void onClick() {
-                        setResponsePage(ShowUser.class,
-                                new PageParameters("id=" + b.getFelhasznalo().getId().toString()));
-                    }
-                };
-                felhasznaloLink.add(new Label("felhNev", new PropertyModel(b, "felhasznalo.nev")));
-                item.add(felhasznaloLink);
-                item.add(new Label("belepotipus"));
-                item.add(new Label("szovegesErtekeles"));
-            }
-        };
-        belepoTable.add(belepoListView);
-        add(belepoTable);
-    }
-
-    public void updatePage() {
-        if (selectedErtekeles != null) {
-            pontIgenyek.clear();
-            pontIgenyek.addAll(ertekelesManager.findPontIgenyekForErtekeles(selectedErtekeles.getId()));
-            belepoIgenyek.clear();
-            belepoIgenyek.addAll(ertekelesManager.findBelepoIgenyekForErtekeles(selectedErtekeles.getId()));
-            setHeaderLabelText(csoport.getNev() + " részletes pontozásai");
-        }
+        ertekelesPanel = new ErtekelesDetailPanel("ertekelesinfo");
+        ertekelesPanel.updateDatas(selectedErtekeles);
+        ertekelesPanel.setVisible(false);
+        add(ertekelesPanel);
     }
 }
