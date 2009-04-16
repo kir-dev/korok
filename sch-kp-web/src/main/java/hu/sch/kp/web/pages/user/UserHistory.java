aba9e3c9b75acbeb3e66db400bba47dab5d226f3
halacs <halacs@81257025-f8dc-45f4-8694-d92bdbe95a3d>
2009-04-16 18:23:06 +0000

    Szemeszterenkénti pont összegzés; See ticket: #160
    git-svn-id: https://maszat.sch.bme.hu/svn/vir-dev/trunk/sch-kp@551 81257025-f8dc-45f4-8694-d92bdbe95a3d

--- sch-kp-web/src/main/java/hu/sch/kp/web/pages/user/UserHistory.java
+++ sch-kp-web/src/main/java/hu/sch/kp/web/pages/user/UserHistory.java
@@ -7,10 +7,12 @@ package hu.sch.kp.web.pages.user;
 import hu.sch.domain.BelepoIgeny;
 import hu.sch.domain.Felhasznalo;
 import hu.sch.domain.PontIgeny;
+import hu.sch.domain.Szemeszter;
 import hu.sch.kp.services.UserManagerLocal;
 import hu.sch.kp.web.pages.index.Index;
 import hu.sch.kp.web.session.VirSession;
 import hu.sch.kp.web.templates.SecuredPageTemplate;
+import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.EJB;
 import org.apache.wicket.PageParameters;
@@ -24,51 +26,118 @@ import org.apache.wicket.model.CompoundPropertyModel;
  *
  * @author Adam Lantos
  */
-public class UserHistory extends SecuredPageTemplate {
+public class UserHistory extends SecuredPageTemplate
+{
 
     @EJB(name = "UserManagerBean")
     UserManagerLocal userManager;
     Long id;
     private boolean own_profile = false;
 
-    public UserHistory() {
+    public UserHistory()
+    {
         own_profile = true;
         initComponents();
     }
 
-    public UserHistory(PageParameters parameters) {
-        try {
+    public UserHistory(PageParameters parameters)
+    {
+        try
+        {
             id = parameters.getLong("id");
-        } catch (Throwable t) {
+        }
+        catch (Throwable t)
+        {
             t.printStackTrace();
         }
         initComponents();
     }
 
-    private void initComponents() {
-        if (id == null) {
+    private void initComponents()
+    {
+        if (id == null)
+        {
             id = ((VirSession) getSession()).getUser().getId();
         }
-        if (id == null) {
+        if (id == null)
+        {
             setResponsePage(Index.class);
             return;
         }
 
         Felhasznalo user = userManager.findUserById(id);
         setHeaderLabelText(user.getNev() + " részletes közösségi története");
-        if (own_profile) {
+        if (own_profile)
+        {
             add(new BookmarkablePageLink("simpleView", ShowUser.class));
-        } else {
-            add(new BookmarkablePageLink("simpleView", ShowUser.class,
-                    new PageParameters("id=" + user.getId().toString())));
+        }
+        else
+        {
+            add(new BookmarkablePageLink("simpleView", ShowUser.class, new PageParameters("id=" + user.getId().toString())));
         }
         setModel(new CompoundPropertyModel(user));
 
         List<PontIgeny> pontIgenyek = userManager.getPontIgenyekForUser(user);
-        ListView plv = new ListView("pontigeny", pontIgenyek) {
+
+        // Szemeszterenkénti pontigények táblázat
+        ArrayList<SzemeszterPont> szemeszterPontok = new ArrayList<SzemeszterPont>();
+
+        // szummázás félévente...
+        Szemeszter szemeszter = null;
+
+        for (PontIgeny pontIgeny : pontIgenyek)
+        {
+            szemeszter = pontIgeny.getErtekeles().getSzemeszter(); // ezt a szemesztert fogom most számolni
+
+            // megnézem megszámoltam-e már ezt a szemesztert
+            boolean next = false;
+            for (SzemeszterPont szemeszterPont : szemeszterPontok)
+            {
+                if (szemeszterPont.getSzemeszter().toString().equals(szemeszter.toString()))
+                {
+                    next = true;
+                    break;
+                }
+            }
+            if (next)
+            {
+                continue;
+            }
+
+            // az aktuális szemeszterre kapott pontokat összegzem
+            int pont = 0;
+            for (PontIgeny szemeszterPont : pontIgenyek)
+            {
+                if (szemeszterPont.getErtekeles().getSzemeszter().toString().equals(szemeszter.toString()))
+                {
+                    pont = pont + szemeszterPont.getPont();
+                }
+            }
+
+            // az eredményt berakom egy listába
+            szemeszterPontok.add(new SzemeszterPont(szemeszter, pont));
+        }
+
+        // megjelenítés...
+        ListView splv = new ListView("szemeszterPont", szemeszterPontok)
+        {
+            @Override
+            protected void populateItem(ListItem item)
+            {
+                SzemeszterPont p = (SzemeszterPont) item.getModelObject();
+                item.add(new Label("szemeszterPont.szemeszter", p.getSzemeszter().toString()));
+                item.add(new Label("szemeszterPont.pont", String.valueOf(p.getPont())));
+            }
+        };
+        add(splv);
+
+        // Pontigények táblázat
+        ListView plv = new ListView("pontigeny", pontIgenyek)
+        {
 
             @Override
-            protected void populateItem(ListItem item) {
+            protected void populateItem(ListItem item)
+            {
                 item.setModel(new CompoundPropertyModel(item.getModelObject()));
                 item.add(new Label("ertekeles.szemeszter"));
                 item.add(new Label("ertekeles.csoport.nev"));
@@ -77,12 +146,14 @@ public class UserHistory extends SecuredPageTemplate {
         };
         add(plv);
 
-        List<BelepoIgeny> belepoIgenyek =
-                userManager.getBelepoIgenyekForUser(user);
-        ListView blv = new ListView("belepoigeny", belepoIgenyek) {
+        // Belépő igények táblázat
+        List<BelepoIgeny> belepoIgenyek = userManager.getBelepoIgenyekForUser(user);
+        ListView blv = new ListView("belepoigeny", belepoIgenyek)
+        {
 
             @Override
-            protected void populateItem(ListItem item) {
+            protected void populateItem(ListItem item)
+            {
                 item.setModel(new CompoundPropertyModel(item.getModelObject()));
                 item.add(new Label("ertekeles.szemeszter"));
                 item.add(new Label("ertekeles.csoport.nev"));
@@ -91,5 +162,28 @@ public class UserHistory extends SecuredPageTemplate {
             }
         };
         add(blv);
+    }
+}
+
+class SzemeszterPont
+{
+
+    private Szemeszter szemeszter;
+    private int pont;
+
+    public SzemeszterPont(Szemeszter szemeszter, int pont)
+    {
+        this.szemeszter = szemeszter;
+        this.pont = pont;
+    }
+
+    public int getPont()
+    {
+        return pont;
+    }
+
+    public Szemeszter getSzemeszter()
+    {
+        return szemeszter;
     }
 }
