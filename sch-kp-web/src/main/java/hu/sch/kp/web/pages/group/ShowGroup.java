4cc511b1ca06c9b040ec4332c3e73618eae17b92
aldaris <aldaris@81257025-f8dc-45f4-8694-d92bdbe95a3d>
2009-04-20 03:08:36 +0000

    Separated lists for groups. See ticket: #134
    git-svn-id: https://maszat.sch.bme.hu/svn/vir-dev/trunk/sch-kp@574 81257025-f8dc-45f4-8694-d92bdbe95a3d

--- sch-kp-web/src/main/java/hu/sch/kp/web/pages/group/ShowGroup.java
+++ sch-kp-web/src/main/java/hu/sch/kp/web/pages/group/ShowGroup.java
@@ -12,6 +12,9 @@ import hu.sch.kp.web.components.FelhasznaloLink;
 import hu.sch.kp.web.pages.index.Index;
 import hu.sch.kp.web.pages.user.ShowUser;
 import hu.sch.kp.web.templates.SecuredPageTemplate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import org.apache.wicket.PageParameters;
 import org.apache.wicket.datetime.markup.html.basic.DateLabel;
 import org.apache.wicket.extensions.markup.html.basic.SmartLinkLabel;
@@ -31,7 +34,6 @@ public class ShowGroup extends SecuredPageTemplate {
 
 //    @EJB(name = "LDAPPersonManagerBean")
 //    LdapPersonManagerLocal ldapManager;
-
     public ShowGroup(PageParameters parameters) {
         Object p = parameters.get("id");
         Long id = null;
@@ -45,7 +47,7 @@ public class ShowGroup extends SecuredPageTemplate {
         Csoport cs = userManager.findGroupWithCsoporttagsagokById(id);
         Felhasznalo user = userManager.findUserWithCsoporttagsagokById(getSession().getUser().getId());
         if (cs == null) {
-            info("Nem vagy körtag");
+            info("Ilyen kör nem létezik");
             setResponsePage(GroupHierarchy.class);
             return;
         }
@@ -72,8 +74,26 @@ public class ShowGroup extends SecuredPageTemplate {
         add(new SmartLinkLabel("levelezoLista"));
         add(new MultiLineLabel("leiras"));
         cs.sortCsoporttagsagok();
-        ListView csoptagsagok = new ListView("csoptagsag",
-                cs.getCsoporttagsagok()) {
+        List<Csoporttagsag> activeMembers = cs.getCsoporttagsagok();
+
+        int low = 0;
+        int high = activeMembers.size() - 1;
+        int mid = high + 1;
+        while (low <= high) {
+            mid = (low + high) / 2;
+            if (activeMembers.get(mid).getVeg() == null) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        List<Csoporttagsag> inactiveMembers =
+                new ArrayList<Csoporttagsag>(activeMembers.subList(mid, activeMembers.size()));
+        if (inactiveMembers.get(0).getVeg() == null) {
+            inactiveMembers = new ArrayList<Csoporttagsag>();
+        }
+        activeMembers.removeAll(inactiveMembers);
+        ListView csoptagsagok = new ListView("csoptagsag", activeMembers) {
 
             @Override
             protected void populateItem(ListItem item) {
@@ -88,5 +108,18 @@ public class ShowGroup extends SecuredPageTemplate {
             }
         };
         add(csoptagsagok);
+        ListView oregtagsagok = new ListView("oregtagsag", inactiveMembers) {
+
+            @Override
+            protected void populateItem(ListItem item) {
+                Csoporttagsag cs = (Csoporttagsag) item.getModelObject();
+                item.setModel(new CompoundPropertyModel(cs));
+                item.add(new FelhasznaloLink("felhlink", cs.getFelhasznalo()));
+                item.add(new Label("becenev", cs.getFelhasznalo().getBecenev()));
+                item.add(DateLabel.forDatePattern("kezdet", "yyyy.MM.dd."));
+                item.add(DateLabel.forDatePattern("veg", "yyyy.MM.dd."));
+            }
+        };
+        add(oregtagsagok);
     }
 }
