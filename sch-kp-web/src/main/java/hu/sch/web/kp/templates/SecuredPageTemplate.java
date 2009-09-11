4d475ab39b0cb53724685fb24bb494bfa3d24d41
Peter Major <majorpetya@gmail.com>
2009-09-11 16:24:18 +0200

    Disabling development mode...

--- sch-kp-web/src/main/java/hu/sch/web/kp/templates/SecuredPageTemplate.java
+++ sch-kp-web/src/main/java/hu/sch/web/kp/templates/SecuredPageTemplate.java
@@ -27,7 +27,6 @@ import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
-import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 
 /**
