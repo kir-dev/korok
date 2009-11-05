e3ffaddc7482d764d97216ab7b72d65ba8e39bdd
Peter Major <majorpetya@gmail.com>
2009-11-06 00:52:20 +0100

    Fixing typo in query

--- sch-kp-ejb-impl/src/main/java/hu/sch/ejb/UserManagerBean.java
+++ sch-kp-ejb-impl/src/main/java/hu/sch/ejb/UserManagerBean.java
@@ -313,7 +313,7 @@ public class UserManagerBean implements UserManagerLocal {
 
     @Override
     public Membership getMembership(final Long groupId, final Long userId) {
-        Query q = em.createQuery("SELET ms FROM Membership ms WHERE ms.user.id = :userId " +
+        Query q = em.createQuery("SELECT ms FROM Membership ms WHERE ms.user.id = :userId " +
                 "AND ms.group.id = :groupId");
         q.setParameter("groupId", groupId);
         q.setParameter("userId", userId);
