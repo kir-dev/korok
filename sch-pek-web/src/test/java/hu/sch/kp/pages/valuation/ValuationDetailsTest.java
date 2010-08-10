de53f832a21cf0250ac2f700c477d738d7cba39f
Bálint Kriván <balint@krivan.hu>
2010-08-10 22:04:28 +0200

    ValuationDetailsTest fix. Akkor is menjen, ha nincsen elbírálási időszak.

--- sch-pek-web/src/test/java/hu/sch/kp/pages/valuation/ValuationDetailsTest.java
+++ sch-pek-web/src/test/java/hu/sch/kp/pages/valuation/ValuationDetailsTest.java
@@ -30,17 +30,19 @@
  */
 package hu.sch.kp.pages.valuation;
 
-import java.util.List;
 import hu.sch.domain.Valuation;
+import hu.sch.domain.ValuationPeriod;
 import hu.sch.domain.ValuationStatistic;
+import hu.sch.ejb.SystemManagerBean;
 import hu.sch.ejb.ValuationManagerBean;
+import hu.sch.services.SystemManagerLocal;
 import hu.sch.services.ValuationManagerLocal;
 import hu.sch.web.kp.valuation.ValuationDetails;
 import hu.sch.web.test.WebTest;
+import java.util.List;
 import javax.naming.NamingException;
 import org.apache.wicket.PageParameters;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
@@ -51,21 +53,26 @@ public class ValuationDetailsTest extends WebTest {
 
     @Test
     public void render() {
-        // kérjük le a statisztikákat
-        ValuationManagerLocal valuationManager = null;
         try {
-            valuationManager = lookupEJB(ValuationManagerBean.class);
+            SystemManagerLocal systemManager = lookupEJB(SystemManagerBean.class);
+            // elmentjük az előző időszakot
+            ValuationPeriod prevIdoszak = systemManager.getErtekelesIdoszak();
+            // mindenképpen legyen elbírálási időszak
+            systemManager.setErtekelesIdoszak(ValuationPeriod.ERTEKELESELBIRALAS);
+
+            // kérjük le a statisztikákat
+            ValuationManagerLocal valuationManager = lookupEJB(ValuationManagerBean.class);
+            List<ValuationStatistic> list = valuationManager.findValuationStatisticForSemester();
+            if (!list.isEmpty()) {
+                // válasszunk ki belőle az elsőt és nézzük meg, hogy lerenderelhető-e
+                Valuation val = list.get(0).getValuation();
+                tester.startPage(ValuationDetails.class, new PageParameters("id=" + val.getId()));
+                tester.assertRenderedPage(ValuationDetails.class);
+            }
+
+            systemManager.setErtekelesIdoszak(prevIdoszak);
         } catch (NamingException ne) {
             ne.printStackTrace();
-        }
-        assertNotNull(valuationManager);
-
-        List<ValuationStatistic> list = valuationManager.findValuationStatisticForSemester();
-        if (!list.isEmpty()) {
-            // válasszunk ki belőle az elsőt és nézzük meg, hogy lerenderelhető-e
-            Valuation val = list.get(0).getValuation();
-            tester.startPage(ValuationDetails.class, new PageParameters("id=" + val.getId()));
-            tester.assertRenderedPage(ValuationDetails.class);
         }
     }
 }
