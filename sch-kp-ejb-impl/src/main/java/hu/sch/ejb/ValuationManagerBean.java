90315c2b2dcd5997a3330b97052f8379909dc626
Peter Major <majorpetya@sch.bme.hu>
2010-09-21 00:28:52 +0200

    Ez kimaradt Csak tudnám, hogy akkor eddig hogyan fordult le :)

--- sch-kp-ejb-impl/src/main/java/hu/sch/ejb/ValuationManagerBean.java
+++ sch-kp-ejb-impl/src/main/java/hu/sch/ejb/ValuationManagerBean.java
@@ -68,6 +68,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 import org.apache.log4j.Logger;
 
 /**
