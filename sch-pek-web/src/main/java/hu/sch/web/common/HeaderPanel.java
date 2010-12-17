5882f534740ea5eb859bb2f192841263bacdd64d
Peter Major <majorpetya@sch.bme.hu>
2010-12-18 00:10:10 +0100

    #960 javítása Most már a profilon is van keresési mező, bár elég érdekes, hogy submit után a user átkerül a körökre... Kéne nagyon az az egységes design.

--- sch-pek-web/src/main/java/hu/sch/web/common/HeaderPanel.java
+++ sch-pek-web/src/main/java/hu/sch/web/common/HeaderPanel.java
@@ -28,15 +28,23 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 package hu.sch.web.common;
 
+import hu.sch.web.kp.search.SearchResultsPage;
+import java.util.ArrayList;
 import java.util.List;
+import org.apache.wicket.PageParameters;
+import org.apache.wicket.RestartResponseException;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.form.StatelessForm;
+import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.model.PropertyModel;
 
 /**
  * Minden {@link PekPage} sablonnak implementálnia kell a {@link PekPage#getHeaderPanel(java.lang.String)}
@@ -49,8 +57,49 @@ import org.apache.wicket.markup.html.panel.Panel;
  */
 public abstract class HeaderPanel extends Panel {
 
+    private String searchTerm;
+    private String searchType = "felhasználó";
+
     public HeaderPanel(String id) {
         super(id);
+        createSearchBar();
+    }
+
+    private void createSearchBar() {
+        StatelessForm<Void> searchForm = new StatelessForm<Void>("searchForm") {
+
+            @Override
+            protected void onSubmit() {
+                if (searchType == null || searchTerm == null) {
+                    super.getSession().error("Hibás keresési feltétel!");
+                    throw new RestartResponseException(getApplication().getHomePage());
+                }
+                if (searchTerm.length() < 3) {
+                    super.getSession().error("Túl rövid keresési feltétel!");
+                    throw new RestartResponseException(getApplication().getHomePage());
+                }
+                PageParameters params = new PageParameters();
+                params.put("type", ((searchType.equals("felhasználó")) ? "user" : "group"));
+                params.put("key", searchTerm);
+                setResponsePage(SearchResultsPage.class, params);
+            }
+        };
+        DropDownChoice<String> searchTypeDdc = new DropDownChoice<String>("searchDdc",
+                new PropertyModel<String>(this, "searchType"),
+                new LoadableDetachableModel<List<? extends String>>() {
+
+                    @Override
+                    protected List<? extends String> load() {
+                        List<String> ret = new ArrayList<String>();
+                        ret.add("felhasználó");
+                        ret.add("kör");
+                        return ret;
+                    }
+                });
+        searchTypeDdc.setNullValid(false);
+        searchForm.add(searchTypeDdc);
+        searchForm.add(new TextField<String>("searchField", new PropertyModel<String>(this, "searchTerm")));
+        add(searchForm);
     }
 
     /**
