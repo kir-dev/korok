7accd9e6a0269ee96e4a84a304d999e2d9189585
Balazs Varga <balo.software@gmail.com>
2012-05-17 01:32:04 +0200

    tinymce workaround: 1.4.20-tól a SecurePackageResourceGuard már nem engedi meg a htm fájlokat

--- sch-pek-web/src/main/java/hu/sch/web/PhoenixApplication.java
+++ sch-pek-web/src/main/java/hu/sch/web/PhoenixApplication.java
@@ -1,32 +1,29 @@
 /**
- * Copyright (c) 2008-2010, Peter Major
- * All rights reserved.
+ * Copyright (c) 2008-2010, Peter Major All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * * Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- *  * Redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in the
- * documentation and/or other materials provided with the distribution.
- *  * Neither the name of the Peter Major nor the
- * names of its contributors may be used to endorse or promote products
- * derived from this software without specific prior written permission.
- *  * All advertising materials mentioning features or use of this software
- * must display the following acknowledgement:
- * This product includes software developed by the Kir-Dev Team, Hungary
- * and its contributors.
+ * modification, are permitted provided that the following conditions are met: *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer. * Redistributions in binary
+ * form must reproduce the above copyright notice, this list of conditions and
+ * the following disclaimer in the documentation and/or other materials provided
+ * with the distribution. * Neither the name of the Peter Major nor the names of
+ * its contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission. * All advertising
+ * materials mentioning features or use of this software must display the
+ * following acknowledgement: This product includes software developed by the
+ * Kir-Dev Team, Hungary and its contributors.
  *
- * THIS SOFTWARE IS PROVIDED BY Peter Major ''AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL Peter Major BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY Peter Major ''AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL Peter Major BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package hu.sch.web;
 
@@ -79,6 +76,7 @@ import javax.ejb.EJB;
 import org.apache.log4j.Logger;
 import org.apache.wicket.*;
 import org.apache.wicket.injection.web.InjectorHolder;
+import org.apache.wicket.markup.html.SecurePackageResourceGuard;
 import org.apache.wicket.protocol.http.*;
 import org.apache.wicket.request.target.coding.HybridUrlCodingStrategy;
 import org.apache.wicket.resource.ContextRelativeResource;
@@ -109,8 +107,7 @@ public class PhoenixApplication extends WebApplication {
 
     @Override
     public Class<? extends Page> getHomePage() {
-        StringBuffer url = ((WebRequest)RequestCycle.get().getRequest()).getHttpServletRequest()
-                .getRequestURL();
+        StringBuffer url = ((WebRequest) RequestCycle.get().getRequest()).getHttpServletRequest().getRequestURL();
 
         if (url.toString().contains("profile")) {
             return ShowPersonPage.class;
@@ -121,11 +118,10 @@ public class PhoenixApplication extends WebApplication {
 
     /**
      * Mivel nem elég a jelenleg támogatott kétféle configurationType
-     * (DEPLOYMENT és DEVELOPMENT), ezért
-     * felüldefiniáljuk ezt a metódust és az {@link Environment}-től függően térünk vissza
-     * az előbbiek valamelyikével.
+     * (DEPLOYMENT és DEVELOPMENT), ezért felüldefiniáljuk ezt a metódust és az {@link Environment}-től
+     * függően térünk vissza az előbbiek valamelyikével.
      *
-     * @return  DEPLOYMENT vagy DEVELOPMENT
+     * @return DEPLOYMENT vagy DEVELOPMENT
      */
     @Override
     public String getConfigurationType() {
@@ -178,6 +174,11 @@ public class PhoenixApplication extends WebApplication {
 
         InjectorHolder.getInjector().inject(this);
         isNewbieTime = systemManager.getNewbieTime();
+
+        //tinymce workaround. see: https://github.com/wicketstuff/core/issues/113
+        SecurePackageResourceGuard guard =
+                (SecurePackageResourceGuard) getResourceSettings().getPackageResourceGuard();
+        guard.addPattern("+*.htm");
 
         log.warn("Application has been successfully initiated");
     }
