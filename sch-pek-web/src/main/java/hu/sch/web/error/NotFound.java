584e470323b77b9b9af52185575321a060b85f6a
Balazs Varga <balo.software@gmail.com>
2013-07-21 15:15:20 +0200

    Fine tune the custom 404 page

--- sch-pek-web/src/main/java/hu/sch/web/error/NotFound.java
+++ sch-pek-web/src/main/java/hu/sch/web/error/NotFound.java
@@ -1,6 +1,8 @@
 package hu.sch.web.error;
 
 import hu.sch.web.kp.KorokPage;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.wicket.request.http.WebResponse;
 
 /**
  *
@@ -12,5 +14,20 @@ public final class NotFound extends KorokPage {
         super();
         setHeaderLabelText("Hiba!");
     }
-}
 
+    @Override
+    protected void configureResponse(final WebResponse response) {
+        super.configureResponse(response);
+        response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    }
+
+    @Override
+    public boolean isVersioned() {
+        return false;
+    }
+
+    @Override
+    public boolean isErrorPage() {
+        return true;
+    }
+}
