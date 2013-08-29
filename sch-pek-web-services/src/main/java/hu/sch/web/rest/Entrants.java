81d54931fa2af8118ebc6e45dd3ed8f87d1dba65
Balazs Varga <balo.software@gmail.com>
2013-08-29 22:33:51 +0200

    Refactor entrants webservice: extract common stuff to an abstract class * common supperclass for web services like #49 * use patterns from the PatternHolder class

--- sch-pek-web-services/src/main/java/hu/sch/web/rest/Entrants.java
+++ sch-pek-web-services/src/main/java/hu/sch/web/rest/Entrants.java
@@ -1,9 +1,9 @@
 package hu.sch.web.rest;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import hu.sch.domain.Semester;
 import hu.sch.domain.rest.ApprovedEntrant;
+import hu.sch.domain.util.PatternHolder;
 import hu.sch.services.ValuationManagerLocal;
 import hu.sch.services.exceptions.PersonNotFoundException;
 import java.util.LinkedList;
@@ -14,12 +14,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.ResponseBuilder;
-import javax.ws.rs.core.UriInfo;
 import org.apache.log4j.Logger;
 
 /**
@@ -27,15 +23,12 @@ import org.apache.log4j.Logger;
  * @author balo
  */
 @Path("/entrants")
-@ManagedBean
-public class Entrants {
+@ManagedBean(value = "EntrantsRestBean")
+public class Entrants extends PekWebservice {
 
     private static final Logger LOGGER = Logger.getLogger(Entrants.class);
     @EJB
     private ValuationManagerLocal valuationManager;
-    @Context
-    private UriInfo context;
-    private static ObjectMapper mapper = new ObjectMapper();
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -46,62 +39,35 @@ public class Entrants {
 
         doAudit();
 
-        if (!semesterId.matches("[0-9]{9}")) {
+        if (!PatternHolder.SEMESTER_PATTERN.matcher(semesterId).matches()) {
             LOGGER.error("Webservice called with invalid semesterid=" + semesterId);
-
-            triggerErrorResponse(Response.Status.BAD_REQUEST,
-                    "Semester must be 9 digit characters, ex.: 200820091; given=" + semesterId);
+            triggerErrorResponse(Response.Status.BAD_REQUEST);
         }
 
-        if (!neptun.matches("[a-zA-Z0-9]{6}")) {
+        if (!PatternHolder.NEPTUN_PATTERN.matcher(neptun).matches()) {
             LOGGER.error("Webservice called with invalid neptun=" + neptun);
-
-            triggerErrorResponse(Response.Status.BAD_REQUEST,
-                    "Neptun must be match [a-zA-Z0-9]{6}, ex.: abc123; given=" + neptun);
+            triggerErrorResponse(Response.Status.BAD_REQUEST);
         }
 
         final List<ApprovedEntrant> entrants = new LinkedList<ApprovedEntrant>();
         try {
             entrants.addAll(valuationManager.getApprovedEntrants(neptun, new Semester(semesterId)));
         } catch (PersonNotFoundException ex) {
-            final String logMsg =
-                    new StringBuilder("Person not found with neptun code=").append(neptun).toString();
-
-            LOGGER.info(logMsg, ex);
-            triggerErrorResponse(Response.Status.NOT_FOUND, logMsg);
+            LOGGER.info("Person not found with neptun code=" + neptun);
+            triggerErrorResponse(Response.Status.NOT_FOUND);
         }
 
-        String s = "";
+        String resultInJson = "";
         try {
-            s = mapper.writeValueAsString(entrants);
+            resultInJson = mapper.writeValueAsString(entrants);
         } catch (JsonProcessingException ex) {
             final String errorMsg = "Couldn't process list to json; given values: neptun="
                     + neptun + ";semester=" + semesterId;
 
             LOGGER.error(errorMsg, ex);
-            triggerErrorResponse(Response.Status.INTERNAL_SERVER_ERROR, errorMsg);
+            triggerErrorResponse(Response.Status.INTERNAL_SERVER_ERROR);
         }
 
-        return s;
-    }
-
-    private void triggerErrorResponse(final Response.Status status, final String msg) {
-        final StringBuilder errorMsg = new StringBuilder("{'error': '").append(msg).append("'}");
-
-        final ResponseBuilder resp = Response.status(status).entity(errorMsg.toString());
-
-        throw new WebApplicationException(resp.build());
-    }
-
-    private void doAudit() {
-        final StringBuilder auditMessage = new StringBuilder("AUDIT LOG for GET method. ");
-        auditMessage.append(" URL: ");
-        if (context != null && context.getRequestUri() != null) {
-            auditMessage.append(context.getRequestUri().toString());
-        } else {
-            LOGGER.info("URIContext or RequestUri was null.");
-            auditMessage.append("UNKNOWN");
-        }
-        LOGGER.info(auditMessage.toString());
+        return resultInJson;
     }
 }
