4c82715eae4c9616cf8993466e22ab01be42e8ab
Balazs Varga <balo.software@gmail.com>
2013-08-29 22:33:51 +0200

    Refactor entrants webservice: extract common stuff to an abstract class * common supperclass for web services like #49 * use patterns from the PatternHolder class

---
+++ sch-pek-web-services/src/main/java/hu/sch/web/rest/PekWebservice.java
@@ -0,0 +1,40 @@
+package hu.sch.web.rest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author balo
+ */
+public abstract class PekWebservice {
+
+    private static final Logger LOGGER = Logger.getLogger(PekWebservice.class);
+    @Context
+    private UriInfo context;
+    protected ObjectMapper mapper = new ObjectMapper();
+
+    protected void doAudit() {
+        final StringBuilder auditMessage = new StringBuilder("AUDIT LOG for GET method. ");
+        auditMessage.append(" URL: ");
+        if (context != null && context.getRequestUri() != null) {
+            auditMessage.append(context.getRequestUri().toString());
+        } else {
+            LOGGER.info("URIContext or RequestUri was null.");
+            auditMessage.append("UNKNOWN");
+        }
+        LOGGER.info(auditMessage.toString());
+    }
+
+    protected void triggerErrorResponse(final Response.Status status) {
+//        final StringBuilder errorMsg = new StringBuilder("{'error': '").append(msg).append("'}");
+        final Response.ResponseBuilder resp = Response.status(status);
+//        .entity(errorMsg.toString());
+
+        throw new WebApplicationException(resp.build());
+    }
+}
