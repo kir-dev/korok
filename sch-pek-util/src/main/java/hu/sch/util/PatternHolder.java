96a80d13526423c8cff55f5f9bae800baaacefed
Balazs Varga <balo.software@gmail.com>
2013-08-29 22:33:34 +0200

    Add new patterns to PatternHolder, clarify neptun patterns

--- sch-pek-util/src/main/java/hu/sch/util/PatternHolder.java
+++ sch-pek-util/src/main/java/hu/sch/util/PatternHolder.java
@@ -13,20 +13,26 @@ public class PatternHolder {
     public static final Pattern GROUP_NAME_OR_POSTTYPE_PATTERN;
     public static final Pattern IM_PATTERN;
     public static final Pattern NAME_PATTERN;
+    public static final Pattern NEPTUN_URN_PATTERN;
     public static final Pattern NEPTUN_PATTERN;
     public static final Pattern PHONE_NUMBER_PATTERN;
     public static final Pattern UID_PATTERN;
     public static final Pattern VIRID_PATTERN;
     public static final Pattern EDUCATION_ID_PATTERN;
+    public static final Pattern SEMESTER_PATTERN;
 
     static {
         //                                                         jog:csoportnév:csoportid
         ENTITLEMENT_PATTERN = Pattern.compile("^.*:entitlement:([^:]+):([^:]+):([0-9]+)$");
         GRADUATION_YEAR_PATTERN = Pattern.compile("[0-9]{8}/[1-2]");
+        SEMESTER_PATTERN = Pattern.compile("^[0-9]{8}[1-2]$");
         GROUP_NAME_OR_POSTTYPE_PATTERN = Pattern.compile("[^|:]*");
         IM_PATTERN = Pattern.compile("^([a-zA-Z]+):(.*)");
         NAME_PATTERN = Pattern.compile("^[A-ZÁÉÍÓÖŐÚÜŰ][a-záéíóöőúüű]+([ \\-][A-ZÁÉÍÓÖŐÚÜŰ][a-záéíóöőúüű]+)*$");
-        NEPTUN_PATTERN = Pattern.compile("^.*:([A-Za-z0-9]{6,7})$");
+        //according to the neptun code spec., neptun codes can be 7 chars length!
+        NEPTUN_PATTERN = Pattern.compile("^[A-Z0-9]{6,7}$");
+        NEPTUN_URN_PATTERN = Pattern.compile("^.*:([A-Za-z0-9]{6,7})$");
+        //
         PHONE_NUMBER_PATTERN = Pattern.compile(".*\\d.*");
         UID_PATTERN = Pattern.compile("^[a-z0-9]*$");
         VIRID_PATTERN = Pattern.compile("^.*:([0-9]+)$");
