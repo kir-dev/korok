d7f5a899ae29340024b5d5a423cfa3f515af81b3
Balazs Varga <balo.software@gmail.com>
2013-10-09 23:08:46 +0200

    Rewrite ConfirmPage (fixes #64) * remove redirects to the protected homepage * ensure the page is stateless * proper error and info message display * localization from property files

--- sch-pek-web/src/main/java/hu/sch/web/profile/confirmation/ConfirmPage.java
+++ sch-pek-web/src/main/java/hu/sch/web/profile/confirmation/ConfirmPage.java
@@ -1,27 +1,33 @@
 package hu.sch.web.profile.confirmation;
 
+import hu.sch.domain.config.Configuration;
 import hu.sch.domain.user.User;
-import hu.sch.domain.user.UserStatus;
 import hu.sch.services.exceptions.PekEJBException;
+import hu.sch.web.error.NotFound;
 import hu.sch.web.profile.ProfilePage;
-import java.util.logging.Level;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.wicket.RestartResponseException;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.FormComponentLabel;
 import org.apache.wicket.markup.html.form.PasswordTextField;
+import org.apache.wicket.markup.html.form.SimpleFormComponentLabel;
+import org.apache.wicket.markup.html.form.StatelessForm;
+import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.form.validation.EqualPasswordInputValidator;
 import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.model.ResourceModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.validation.validator.StringValidator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * Confirms a user account. If the user hasn't got a password he can set it on
+ * this page.
  *
  * @author konvergal
  * @author tomi
+ * @author balo
  */
 public final class ConfirmPage extends ProfilePage {
 
@@ -31,61 +37,83 @@ public final class ConfirmPage extends ProfilePage {
     private User user = null;
 
     public ConfirmPage() {
-        throw new RestartResponseException(getApplication().getHomePage());
+        throw new RestartResponseException(NotFound.class);
     }
 
-    public ConfirmPage(PageParameters params) {
-        String confirmationCode = params.get("code").toString("");
+    public ConfirmPage(final PageParameters params) {
+        setHeaderLabelText(getString("headerLabel"));
+
+        final String confirmationCode = params.get("code").toString("");
+        setStatelessHint(true);
 
         if (StringUtils.isBlank(confirmationCode)) {
-            throw new RestartResponseException(getApplication().getHomePage());
+            error(getString("error.missingcode"));
+            return;
         }
 
         user = userManager.findUserByConfirmationCode(confirmationCode);
         if (user == null) {
-            throw new RestartResponseException(getApplication().getHomePage());
+            error(getString("error.wrongcode"));
+            return;
         }
 
         // user has password -> just confirm, nothing else to do
         if (StringUtils.isNotBlank(user.getPasswordDigest())) {
-            confirmAndRedirect(null);
-        } else {
-            addPasswordFields();
+            confirm(null);
         }
+    }
 
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+
+        addPasswordFields();
     }
 
     private void addPasswordFields() {
-        Form<Void> form = new Form<Void>("passwordForm") {
+        final Form<Void> form = new StatelessForm<Void>("passwordForm") {
             @Override
             protected void onSubmit() {
-                confirmAndRedirect(password);
+                if (confirm(password)) {
+                    //hide the form if the confirmation was successful
+                    setVisible(false);
+                }
             }
         };
 
-        PasswordTextField passwordTF = new PasswordTextField("password", new PropertyModel<String>(this, "password"));
-        passwordTF.add(StringValidator.minimumLength(6));
-        FormComponentLabel passwordLabel = new FormComponentLabel("passwordLabel", passwordTF);
+        final TextField passwordTF = new PasswordTextField("password", new PropertyModel<String>(this, "password"));
+        passwordTF
+                .setLabel(new ResourceModel("passwordTF"))
+                .add(StringValidator.minimumLength(6));
+
+        final FormComponentLabel passwordLabel = new SimpleFormComponentLabel("passwordLabel", passwordTF);
         form.add(passwordLabel, passwordTF);
 
-        PasswordTextField passwordConfirmTF = new PasswordTextField("passwordConfirm", new PropertyModel<String>(this, "passwordConfirm"));
-        FormComponentLabel passwordConfirmLabel = new FormComponentLabel("passwordConfirmLabel", passwordConfirmTF);
-        form.add(passwordConfirmLabel, passwordConfirmTF);
+        final TextField passwordConfirmTF = new PasswordTextField("passwordConfirm", new PropertyModel<String>(this, "passwordConfirm"));
+        passwordConfirmTF.setLabel(new ResourceModel("passwordConfirmTF"));
 
-        form.add(new EqualPasswordInputValidator(passwordTF, passwordConfirmTF));
+        final FormComponentLabel passwordConfirmLabel = new SimpleFormComponentLabel("passwordConfirmLabel", passwordConfirmTF);
+
+        form
+                .add(passwordConfirmLabel, passwordConfirmTF)
+                .add(new EqualPasswordInputValidator(passwordTF, passwordConfirmTF))
+                //if we found the user and he hasn't got a password
+                .setVisible(user != null && StringUtils.isBlank(user.getPasswordDigest()));
 
         add(form);
     }
 
-    private void confirmAndRedirect(String password) {
+    private boolean confirm(final String password) {
         try {
             userManager.confirm(user, password);
-            getSession().info("Sikeres megerősítés. Most már be tudsz jelentkezni.");
-            setResponsePage(getApplication().getHomePage());
+            info(String.format(getString("confirm.success"),
+                    Configuration.getProfileDomain()));
+
+            return true;
         } catch (PekEJBException ex) {
-            // TODO: proper error message?
-            getSession().error("Nem sikerült megerősíteni a fehasználód. Kérlek fordulj a supporthoz!");
-            throw new RestartResponseException(getApplication().getHomePage());
+            error(getString("confirm.failed"));
         }
+
+        return false;
     }
 }
