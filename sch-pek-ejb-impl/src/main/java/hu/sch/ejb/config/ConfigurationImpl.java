070d907877c2bf598d614a1c0478ed0815821251
Tamas Michelberger <tomi.michel@gmail.com>
2014-05-14 21:59:00 +0200

    Cleaned up Configuration
    * removed unused configuration options
    * created a separate Environment enum

--- sch-pek-ejb-impl/src/main/java/hu/sch/ejb/config/ConfigurationImpl.java
+++ sch-pek-ejb-impl/src/main/java/hu/sch/ejb/config/ConfigurationImpl.java
@@ -2,10 +2,12 @@ package hu.sch.ejb.config;
 
 import hu.sch.services.config.ImageUploadConfig;
 import hu.sch.services.config.Configuration;
+import hu.sch.services.config.Environment;
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.Properties;
 import javax.enterprise.context.ApplicationScoped;
+import org.apache.commons.lang3.StringUtils;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,15 +25,13 @@ public class ConfigurationImpl implements Configuration {
 
     private static final Logger logger = LoggerFactory.getLogger(ConfigurationImpl.class);
     private static final String PROPERTY_NAME = "application.resource.dir";
-    private static final String TIMES_FONT_FILE = "times.font.file";
+    private static final String ENVIRONMENT = "environment";
     private static final String APPLICATION_FOLDER = "korok";
     private static final String CONFIG_FILE = "config.properties";
     private static final String IMAGE_UPLOAD_PATH = "image.upload.path";
     private static final String IMAGE_MAX_SIZE = "image.upload.max";
     private static final String DOMAIN_PROFILE = "domain.profile";
     private static final String DOMAIN_KOROK = "domain.korok";
-    private static final String SUPPORT_BASE_URL = "support.baseUrl";
-    private static final String SUPPORT_DEFAULT_ID = "support.defaultId";
     private static final String INTERNAL_API_SECRET = "api.secret";
 
     private final Properties properties = new Properties();
@@ -63,11 +63,15 @@ public class ConfigurationImpl implements Configuration {
     @Override
     public Environment getEnvironment() {
         if (environment == null) {
-            String env = properties.getProperty("wicket.configuration", "DEVELOPMENT");
+            // try environment system property first, then the properties file
+            String env = System.getProperty(ENVIRONMENT);
+            if (StringUtils.isBlank(env)) {
+                env = properties.getProperty(ENVIRONMENT, "DEVELOPMENT");
+            }
             try {
                 environment = Environment.valueOf(env);
             } catch (IllegalArgumentException ex) {
-                System.err.println("Illegal 'wicket.configuration' in the config.properties. Fallbacking to DEVELOPMENT.");
+                System.err.println("Illegal environment value. Fallbacking to DEVELOPMENT.");
                 environment = Environment.DEVELOPMENT;
             }
             logger.warn("The application is running in {} mode!", environment.toString());
@@ -88,21 +92,6 @@ public class ConfigurationImpl implements Configuration {
     @Override
     public String getKorokDomain() {
         return properties.getProperty(DOMAIN_KOROK);
-    }
-
-    @Override
-    public String getSupportBaseUrl() {
-        return properties.getProperty(SUPPORT_BASE_URL);
-    }
-
-    @Override
-    public int getSupportDefaultId() {
-        return Integer.parseInt(properties.getProperty(SUPPORT_DEFAULT_ID));
-    }
-
-    @Override
-    public String getFontPath() {
-        return baseDir + APPLICATION_FOLDER + "/" + properties.getProperty(TIMES_FONT_FILE);
     }
 
     @Override
