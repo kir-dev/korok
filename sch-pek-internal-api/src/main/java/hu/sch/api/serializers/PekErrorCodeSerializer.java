4c3d2053c9f01a43b8098597e58d680864b91c68
Tamas Michelberger <tomi.michel@gmail.com>
2014-05-22 22:46:20 +0200

    fixed: PekErrorCode serializer unwraps the error object

--- sch-pek-internal-api/src/main/java/hu/sch/api/serializers/PekErrorCodeSerializer.java
+++ sch-pek-internal-api/src/main/java/hu/sch/api/serializers/PekErrorCodeSerializer.java
@@ -8,19 +8,34 @@ import hu.sch.util.exceptions.PekErrorCode;
 import java.io.IOException;
 
 /**
+ * Serializer for {@link PekErrorCode}. It dumps all the info into the host
+ * object, does not create an enclosing object for the values.
+ *
+ * It must be used in cunjunction with JsonUnwrapped annotation.
+ *
+ * For example:
+ *
+ *    @JsonUnwrapped
+ *    @JsonSerialize(using = PekErrorCodeSerializer.class)
+ *    public PekErrorCode getError() {
+ *        return error;
+ *    }
  *
  * @author tomi
  */
-public final class PekErrorCodeSerializer extends JsonSerializer<PekErrorCode>{
+public final class PekErrorCodeSerializer extends JsonSerializer<PekErrorCode> {
 
     @Override
     public void serialize(PekErrorCode t, JsonGenerator generator, SerializerProvider sp) throws IOException, JsonProcessingException {
-        generator.writeStartObject();
+        // dump values into host object
         generator.writeFieldName("code");
         generator.writeNumber(t.getCode());
         generator.writeFieldName("message");
         generator.writeString(t.getShortMessage());
-        generator.writeEndObject();
     }
 
+    @Override
+    public boolean isUnwrappingSerializer() {
+        return true;
+    }
 }
