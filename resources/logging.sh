3290096b2bcac514745a353ecf1710f08b4cfc21
Tamas Michelberger <tomi.michel@gmail.com>
2014-05-24 00:00:15 +0200

    fixed typo in handler definition (filter-spec)

--- resources/logging.sh
+++ resources/logging.sh
@@ -15,8 +15,8 @@ LOGPATH=$1
 CLIBATCH=$(cat <<BATCH
 /path=pek.log.dir:add(path="$LOGPATH")
 /subsystem=logging/periodic-rotating-file-handler=PEK-ALL:add(append=true, autoflush=true,named-formatter=PATTERN, file={relative-to="pek.log.dir", path="pek-all.log"}, level=INFO, suffix=".yyyy-MM-dd")
-/subsystem=logging/periodic-rotating-file-handler=PEK-WARN:add(append=true, autoflush=true,named-formatter=PATTERN, file={relative-to="pek.log.dir", path="pek-warn.log"}, level=WARN, suffix=".yyyy-MM-dd", filter-spec="level(WARN)")
-/subsystem=logging/periodic-rotating-file-handler=PEK-ERROR:add(append=true, autoflush=true,named-formatter=PATTERN, file={relative-to="pek.log.dir", path="pek-error.log"}, level=ERROR, suffix=".yyyy-MM-dd", filter-spec="level(ERROR)")
+/subsystem=logging/periodic-rotating-file-handler=PEK-WARN:add(append=true, autoflush=true,named-formatter=PATTERN, file={relative-to="pek.log.dir", path="pek-warn.log"}, level=WARN, suffix=".yyyy-MM-dd", filter-spec="levels(WARN)")
+/subsystem=logging/periodic-rotating-file-handler=PEK-ERROR:add(append=true, autoflush=true,named-formatter=PATTERN, file={relative-to="pek.log.dir", path="pek-error.log"}, level=ERROR, suffix=".yyyy-MM-dd", filter-spec="levels(ERROR)")
 /subsystem=logging/logger=hu.sch:add(use-parent-handlers=false, handlers=["PEK-ALL", "PEK-WARN", "PEK-ERROR"])
 BATCH
 )
