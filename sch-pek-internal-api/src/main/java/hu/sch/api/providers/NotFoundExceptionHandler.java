66d0e5dc53812bade811e6d205e07ddcd8fd73b5
Tamas Michelberger <tomi.michel@gmail.com>
2014-05-24 11:57:33 +0200

    'not found' exception handler

---
+++ sch-pek-internal-api/src/main/java/hu/sch/api/providers/NotFoundExceptionHandler.java
@@ -0,0 +1,33 @@
+package hu.sch.api.providers;
+
+import hu.sch.api.response.PekError;
+import hu.sch.util.exceptions.PekErrorCode;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ *
+ * @author tomi
+ */
+@Provider
+public class NotFoundExceptionHandler implements ExceptionMapper<NotFoundException> {
+
+    private static final Logger logger = LoggerFactory.getLogger(NotFoundExceptionHandler.class);
+
+    @Override
+    public Response toResponse(NotFoundException e) {
+        logger.warn("Resource was not found: {}", e.getMessage());
+        return Response
+                .status(Response.Status.NOT_FOUND)
+                .type(MediaType.APPLICATION_JSON_TYPE)
+                .entity(new PekError(PekErrorCode.RESOURCE_NOT_FOUND, e.getMessage()))
+                .build();
+    }
+
+}
