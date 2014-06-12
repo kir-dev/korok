2605f2a856f57b574c5a2e5fe6fb7bd98ea8593b
Szabolcs Varadi <kresshy@gmail.com>
2014-06-12 23:07:09 +0200

    langing page created and almost finished

--- front-end/routes/index.js
+++ front-end/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+      title: 'Profil és Körök - Kir-Dev',
+      username: "Szabolcs Varadi",
+      userimgsrc: "/images/stuff.jpg"
+  });
 });
 
 module.exports = router;
