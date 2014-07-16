8ba14d9c4cec054147ac81dad8aa47d1d2cf8633
Szabolcs Varadi <kresshy@gmail.com>
2014-07-16 10:25:01 +0200

    added log4js as dep and changed logging for this

--- front-end/app.js
+++ front-end/app.js
@@ -12,6 +12,7 @@ var session = require('express-session');
 var params = require('express-params');
 var uuid = require('node-uuid');
 var config = require('./config');
+var log4js = require('log4js');
 
 /// import routing
 var index = require('./routes/index');
@@ -48,6 +49,16 @@ app.use(session({
 
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+
+log4js.configure({
+    appenders: [
+        {type: 'console'},
+        {type: 'file', filename: 'logs/pekFrontendApp.log', category: 'pekFrontendApp'}
+    ]
+});
+
+var logger = log4js.getLogger('pekFrontendApp');
+logger.info('PEK FrontEnd Application is starting up');
 
 //app.use(passport.initialize());
 //app.use(passport.session());
@@ -109,5 +120,5 @@ module.exports = app;
 
 /// running application
 app.listen(app.get('port'), config.hostAddress, function(){
-    console.log('Express server listening on port ' + app.get('port'));
+    logger.info('PEK FrontEnd Application is listening on port' + app.get('port'));
 });
