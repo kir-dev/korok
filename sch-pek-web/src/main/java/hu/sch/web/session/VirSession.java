6d03443e86396697df3c3e5805fb8c55b9d5abec
Tamas Michelberger <tomi.michel@gmail.com>
2014-07-25 00:27:51 +0200

    extended session with oauth specific values

--- sch-pek-web/src/main/java/hu/sch/web/session/VirSession.java
+++ sch-pek-web/src/main/java/hu/sch/web/session/VirSession.java
@@ -10,17 +10,45 @@ import org.apache.wicket.request.Request;
 public class VirSession extends WebSession {
 
     private Long userId;
+    private String oauthUserState;
+    private String accessToken;
 
     public VirSession(Request request) {
         super(request);
     }
 
-    public synchronized Long getUserId() {
+    public static VirSession get() {
+        return (VirSession)WebSession.get();
+    }
+
+    public Long getUserId() {
         return userId;
     }
 
-    public synchronized void setUserId(Long userId) {
+    public void setUserId(Long userId) {
         this.userId = userId;
         dirty();
+    }
+
+    public String getOauthUserState() {
+        return oauthUserState;
+    }
+
+    public void setOauthUserState(String oauthUserState) {
+        this.oauthUserState = oauthUserState;
+        dirty();
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+        dirty();
+    }
+
+    public boolean isUserSignedIn() {
+        return getUserId() != null;
     }
 }
