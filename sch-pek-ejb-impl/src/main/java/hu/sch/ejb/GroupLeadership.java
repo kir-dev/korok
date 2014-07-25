12173ca5c8362dee3f03badf3d39355aa0df630a
Tamas Michelberger <tomi.michel@gmail.com>
2014-07-25 10:34:52 +0200

    group leadership "service"

---
+++ sch-pek-ejb-impl/src/main/java/hu/sch/ejb/GroupLeadership.java
@@ -0,0 +1,33 @@
+package hu.sch.ejb;
+
+import hu.sch.domain.Group;
+import hu.sch.domain.Membership;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+public class GroupLeadership {
+
+    private final EntityManager em;
+
+
+    public GroupLeadership(EntityManager em) {
+        this.em = em;
+    }
+
+    public boolean isLeader(Group group, Long userId) {
+        TypedQuery<Long> q = em.createNamedQuery(Membership.countGroupLeaderInGroup, Long.class);
+        q.setParameter("group", group);
+        q.setParameter("userId", userId);
+
+        return q.getSingleResult() > 0;
+    }
+
+    public boolean hasAny(Long userId) {
+        TypedQuery<Long> q = em.createNamedQuery(Membership.countGroupLeaderShips, Long.class);
+        q.setParameter("userId", userId);
+
+        return q.getSingleResult() > 0;
+    }
+
+
+}
