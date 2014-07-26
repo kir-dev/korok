7d88e8c301dfd49eaaea3f7377aebfe78afd42b2
Tamas Michelberger <tomi.michel@gmail.com>
2014-07-26 14:10:53 +0200

    registration page markup

--- sch-pek-web/src/main/java/hu/sch/web/idm/pages/RegistrationPage.java
+++ sch-pek-web/src/main/java/hu/sch/web/idm/pages/RegistrationPage.java
@@ -1,6 +1,21 @@
 package hu.sch.web.idm.pages;
 
+import hu.sch.services.RegistrationManagerLocal;
+import hu.sch.services.dto.RegisteringUser;
 import hu.sch.web.kp.KorokPage;
+import javax.inject.Inject;
+import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.form.OnChangeAjaxBehavior;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.FormComponentLabel;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.validation.validator.EmailAddressValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -9,12 +24,74 @@ import hu.sch.web.kp.KorokPage;
  */
 public class RegistrationPage extends KorokPage {
 
+    private static final Logger logger = LoggerFactory.getLogger(RegistrationPage.class);
+
+    private RegisteringUser user;
+    private Label screenNameTakenLbl;
+
+    @Inject
+    private RegistrationManagerLocal registrationManager;
+
     public RegistrationPage() {
         setHeaderLabelText("Regisztráció");
+
+        user = new RegisteringUser(getSession().getOAuthUserInfo());
+        add(createForm());
     }
 
     @Override
     protected boolean needsLogin() {
         return false;
+    }
+
+    private void onRegFromSubmit() {
+        // TODO
+        logger.info("User successfully registered.");
+    }
+
+    private Form<RegisteringUser> createForm() {
+        Form<RegisteringUser> form = new Form<RegisteringUser>("regForm", new CompoundPropertyModel<>(user)) {
+
+            @Override
+            protected void onSubmit() {
+                onRegFromSubmit();
+            }
+        };
+
+        addTextField(form, "screenName", true).add(new OnChangeAjaxBehavior() {
+
+            @Override
+            protected void onUpdate(AjaxRequestTarget target) {
+                if (registrationManager.isUidTaken(user.getScreenName())) {
+                    screenNameTakenLbl.add(new AttributeModifier("class", "screen-name-taken"));
+                    screenNameTakenLbl.setDefaultModelObject(getString("screenname.taken"));
+                } else {
+                    screenNameTakenLbl.add(new AttributeModifier("class", "screen-name-ok"));
+                    screenNameTakenLbl.setDefaultModelObject(getString("screenname.ok"));
+                }
+
+                target.add(screenNameTakenLbl);
+            }
+        });
+
+        addTextField(form, "mail", true).add(EmailAddressValidator.getInstance());
+        addTextField(form, "firstName", true);
+        addTextField(form, "lastName", true);
+        addTextField(form, "dormitory", false);
+        addTextField(form, "roomNumber", false);
+
+        form.add(screenNameTakenLbl = new Label("screenNameTakenLbl", Model.of("")));
+        screenNameTakenLbl.setOutputMarkupId(true);
+
+        return form;
+    }
+
+    private TextField<String> addTextField(Form<?> form, String id, boolean required) {
+        TextField<String> textField = new TextField<>(id);
+        textField.setRequired(required);
+        textField.setLabel(Model.of(getString(id)));
+        form.add(textField, new FormComponentLabel(id+"Lbl", textField));
+
+        return textField;
     }
 }
