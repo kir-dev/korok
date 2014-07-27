d5667ceb602ccf2cc480ba8a1a94e43720d98178
Tamas Michelberger <tomi.michel@gmail.com>
2014-07-27 13:36:41 +0200

    send error report on failed user update

--- sch-pek-ejb-impl/src/main/java/hu/sch/ejb/AuthSchUserIntegrationBean.java
+++ sch-pek-ejb-impl/src/main/java/hu/sch/ejb/AuthSchUserIntegrationBean.java
@@ -2,12 +2,16 @@ package hu.sch.ejb;
 
 import hu.sch.domain.user.User;
 import hu.sch.services.AuthSchUserIntegration;
+import hu.sch.services.config.Configuration;
 import hu.sch.services.dto.OAuthUserInfo;
 import java.util.Date;
 import javax.ejb.Asynchronous;
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,6 +23,12 @@ public class AuthSchUserIntegrationBean implements AuthSchUserIntegration {
     @PersistenceContext
     private EntityManager em;
 
+    @Inject
+    private MailManagerBean mailManager;
+
+    @Inject
+    private Configuration config;
+
     @Override
     @Asynchronous
     public void updateUser(Long userId, OAuthUserInfo userInfo) {
@@ -28,6 +38,14 @@ public class AuthSchUserIntegrationBean implements AuthSchUserIntegration {
         user.setLastLoginDate(new Date());
         user.setBmeId(userInfo.getBmeId());
         updateRoomNumber(user, userInfo);
+
+        try {
+            // force flush, so we can catch the exception.
+            em.flush();
+        } catch (PersistenceException ex) {
+            logger.warn(String.format("Could not update user (%d) with data from auth.sch.", userId), ex);
+            sendErrorReport(userId, ex);
+        }
     }
 
     private void updateRoomNumber(User user, OAuthUserInfo userInfo) {
@@ -35,6 +53,18 @@ public class AuthSchUserIntegrationBean implements AuthSchUserIntegration {
             user.setRoom(userInfo.getDormitory().getRoom());
             user.setDormitory(userInfo.getDormitory().getBuildingName());
         }
+    }
+
+    private void sendErrorReport(Long userId, PersistenceException ex) {
+        String subject = MailManagerBean.getMailString(MailManagerBean.MAIL_AUTHSCH_USER_UPDATE_SUBJECT);
+        String body = String.format(
+            MailManagerBean.getMailString(MailManagerBean.MAIL_AUTHSCH_USER_UPDATE_BODY),
+            userId,
+            ex.getMessage(),
+            ExceptionUtils.getStackTrace(ex)
+        );
+
+        mailManager.sendEmail(config.getErrorReportingEmail(), subject, body);
     }
 
 }
